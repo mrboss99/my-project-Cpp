88cabf4ebf7de756f3098a5c30560974006cc3af
mrboss <44489689+mrboss99@users.noreply.github.com>
2018-10-25 23:15:49 -0700

    collect (sum) five integer

---
+++ collect five integers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float i ,sum;
+	for(i=0.5;i<=3.5;i+=0.5){
+		cout<<"number ="<<i<<"\n"<<"\a";	
+	}
+	
+	return 0;
+	cin.get();
+	cin.get();
+}
